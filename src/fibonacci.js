2e21da8a7b974634dfd500730e3058f3a0d46bb7
jackie2806 <jaishackeline@gmail.com>
2024-04-01 21:36:57 -0500

    secuencia

--- src/fibonacci.js
+++ src/fibonacci.js
@@ -7,4 +7,12 @@ function fibonacci(n){
 }
 //fibonacci(3)
 
+console.log(fibonacci(0))
+console.log(fibonacci(1))
+console.log(fibonacci(2))
 console.log(fibonacci(3))
+console.log(fibonacci(4))
+console.log(fibonacci(5))
+console.log(fibonacci(6))
+console.log(fibonacci(7))
+console.log(fibonacci(8))
